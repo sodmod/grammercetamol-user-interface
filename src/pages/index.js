9121f8835887d0feef0b1afa31700d6f42316632
sodmod <66504959+sodmod@users.noreply.github.com>
2024-10-25 12:16:02 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ const Home = lazy(() => import("./home/Home"));
 const Stream = lazy(()=> import("./courses/stream/Stream"));
 const Details = lazy(()=> import("./courses/details/Details"));
 const Courses = lazy(() => import("./courses/courses/Courses"));
-const Overview = lazy(() => import("../components/Overview/Overview"));
+const Overview = lazy(() => import("../components/overview/Overview"));
 const Registered = lazy(() => import("./courses/registered/Registered"));
 const Whitelisted = lazy(() => import("./courses/whitelisted/Whitelisted"));
 const CoursePayment = lazy(()=> import("./courses/payment/CoursePayment.jsx"));
